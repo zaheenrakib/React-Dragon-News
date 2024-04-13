8d910f8f3d0b25b48d04dcd7368fb00ecbdb8d5a
zaheenrakib <rakibislambest@gmail.com>
2024-04-13 11:25:40 +0600

    Added Login Section

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../Shared/Navbar/Navbar';
+
+const Login = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <h2>This is Login</h2>
+        </div>
+    );
+};
+
+export default Login;
